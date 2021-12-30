2a94321693dbc5fe45fa6a603af5c8f15ee20cb8
keyem4251 <kisk.emura@gmail.com>
2021-12-30 09:41:18 +0900

    rename airflow start shell

---
+++ locally/airflow-standalone-start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+source venv/bin/activate
+
+export AIRFLOW_HOME=~/IdeaProjects/airflow-quick-start/locally/airflow
+AIRFLOW_VERSION=2.2.3
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+
+pip install --upgrade pip
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
+airflow standalone
